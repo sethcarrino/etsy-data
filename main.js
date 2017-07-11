ac81b9377af2e8badec81872a031c55ff38286a8
sethcarrino <sethcarrino@gmail.com>
2017-07-10 20:29:34 -0400

    modified main.js

--- main.js
+++ main.js
@@ -7,42 +7,93 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-console.log()
+
 function question1 () {
-  data[ "price" ];
+  let total = 0;
+  let average = 0;
+  for (var i = 0; i < data.length; i++) {
+    if (total = total + data[i].price){
+      average = total / data.length;
+    }
+  }
+console.log("The average price is" + " " + average);
 }
+
+
+
+
 
 
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2 () {
-  // Answer:
+  let array = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].price <= 18 && data[i].price >=14){
+      array.push(data[i].title)
+    }
+
+  }
+  console.log(array);
+
+
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
+
+// 1970s Schlitz Malt Liquor Glass Beer Pitcher costs 18 pounds.
+
 function question3 () {
-  // Answer:
+  let array = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].currency_code === "GBP"){
+    console.log(data[i].title + " costs " + data[i].price + " pounds.");
+    }
+  }
+
 }
+
+
 
 
 // 4: Display a list of all items who are made of wood.
 function question4 () {
-  // Answer:
+  let array = [];
+  for (var i = 0; i < data.length; i++) {
+    if (/wood/i.test(data[i].materials)) {
+      array.push(data[i].title);
+    }
+  }
+  console.log(array);
 }
+
+
 
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5 () {
-  // Answer:
+  let array = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].materials.length >= 8){
+      array.push(data[i].title + " has" + " " + data[i].materials.length + " materials.");
+      array.push(data[i].materials);
+    }
+  }
+  console.log(array);
 }
 
 
-// 6: How many items were made by their sellers?
-// Answer:
+// // 6: How many items were made by their sellers?
+// 18 items were made by their sellers.
+
 function question6 () {
-  // Answer:
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].who_made === "i_did"){
+      console.log(" items were made by their sellers.");
+    }
+  }
 }
